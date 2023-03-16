59f7a1e4f1e1ec2bced9bb50bd2c04c43d091b1b
CedricMlg <malingre.cedric@gmail.com>
2023-03-16 18:39:40 +0100

    Add modal and get the value in the fields

--- src/pages/CreateEmployee/index.jsx
+++ src/pages/CreateEmployee/index.jsx
@@ -1,12 +1,24 @@
 import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
-import { Dropdown } from "cedricm-wealthhealth-dropdown";
+import { Dropdown } from "cedricm_dropdown";
 import DatePicker from "react-datepicker";
+import Modal from "react-modal";
 import "react-datepicker/dist/react-datepicker.css";
 
+Modal.setAppElement("#root");
+
 export default function CreateEmployee() {
+  const [modalIsOpen, setIsOpen] = useState(false);
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [birthDate, setBirthDate] = useState(new Date());
   const [startDate, setStartDate] = useState(new Date());
+  const [street, setStreet] = useState("");
+  const [city, setCity] = useState("");
+  const [state, setState] = useState("");
+  const [zipCode, setZipCode] = useState("");
+  const [department, setDepartment] = useState("");
+  const [isEmpty, setIsEmpty] = useState(true);
 
   const departments = {
     label: "departments",
@@ -260,6 +272,52 @@ export default function CreateEmployee() {
     ],
   };
 
+  function openModal() {
+    setIsOpen(true);
+  }
+
+  function afterOpenModal() {
+    // references are now sync'd and can be accessed.
+    subtitle.style.color = "#f00";
+  }
+
+  function closeModal() {
+    setIsOpen(false);
+  }
+
+  function saveEmployee() {
+    const employee = {
+      firstName: firstName,
+      lastName: lastName,
+      dateOfBirth: birthDate,
+      startDate: startDate,
+      street: street,
+      city: city,
+      state: state,
+      zipCode: zipCode,
+      department: department,
+    };
+
+    if (
+      Object.values(employee).find((element) => element === "") != undefined
+    ) {
+      setIsEmpty(true);
+      openModal();
+      return;
+    }
+
+    setIsEmpty(false);
+    openModal();
+  }
+
+  function handleStateChange(state) {
+    setState(state);
+  }
+
+  function handleDepartmentChange(department) {
+    setDepartment(department);
+  }
+
   return (
     <div className="create-employee">
       <div className="title">
@@ -270,10 +328,18 @@ export default function CreateEmployee() {
         <h2>Create Employee</h2>
         <form action="#" id="create-employee">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" />
+          <input
+            type="text"
+            id="first-name"
+            onChange={(e) => setFirstName(e.target.value)}
+          />
 
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" />
+          <input
+            type="text"
+            id="last-name"
+            onChange={(e) => setLastName(e.target.value)}
+          />
 
           <label htmlFor="date-of-birth">Date of Birth</label>
           <DatePicker
@@ -291,23 +357,57 @@ export default function CreateEmployee() {
             <legend>Address</legend>
 
             <label htmlFor="street">Street</label>
-            <input id="street" type="text" />
+            <input
+              id="street"
+              type="text"
+              onChange={(e) => setStreet(e.target.value)}
+            />
 
             <label htmlFor="city">City</label>
-            <input id="city" type="text" />
+            <input
+              id="city"
+              type="text"
+              onChange={(e) => setCity(e.target.value)}
+            />
 
-            <Dropdown {...states} />
+            <Dropdown {...states} onChange={handleStateChange} />
 
             <label htmlFor="zip-code">Zip Code</label>
-            <input id="zip-code" type="number" />
+            <input
+              id="zip-code"
+              type="number"
+              onChange={(e) => setZipCode(e.target.value)}
+            />
           </fieldset>
 
-          <Dropdown {...departments} />
+          <Dropdown {...departments} onChange={handleDepartmentChange} />
         </form>
 
-        {/* <button onClick="saveEmployee()">Save</button> */}
+        <button onClick={saveEmployee}>Save</button>
       </div>
-      {/* modal */}
+      <Modal
+        isOpen={modalIsOpen}
+        onAfterOpen={afterOpenModal}
+        onRequestClose={closeModal}
+        contentLabel="Response Modal"
+        className="modal"
+      >
+        <button
+          type="button"
+          title="Close modal"
+          className="close-modal"
+          onClick={closeModal}
+        ></button>
+
+        {isEmpty ? (
+          <p>
+            One of the field is empty or wasn't selected, don't forget to select
+            the right state / department even if the preview showed it selected
+          </p>
+        ) : (
+          <p>Employee Created!</p>
+        )}
+      </Modal>
     </div>
   );
 }
