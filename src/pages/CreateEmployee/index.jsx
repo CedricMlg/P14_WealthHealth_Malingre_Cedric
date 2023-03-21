c4b48b565a0e417c0cb05d11ea1e2c9fe5a0e282
CedricMlg <malingre.cedric@gmail.com>
2023-03-21 15:22:51 +0100

    update date picker move data to separate folder

--- src/pages/CreateEmployee/index.jsx
+++ src/pages/CreateEmployee/index.jsx
@@ -3,6 +3,8 @@ import { NavLink } from "react-router-dom";
 import { Dropdown } from "cedricm_dropdown";
 import DatePicker from "react-datepicker";
 import Modal from "react-modal";
+import departments from "../../data/dropdown/departments.json";
+import states from "../../data/dropdown/states.json";
 import "react-datepicker/dist/react-datepicker.css";
 
 Modal.setAppElement("#root");
@@ -20,265 +22,8 @@ export default function CreateEmployee() {
   const [department, setDepartment] = useState("");
   const [isEmpty, setIsEmpty] = useState(true);
 
-  const departments = {
-    label: "departments",
-    options: [
-      { value: "sales", name: "sales" },
-      { value: "marketing", name: "marketing" },
-      { value: "engineering", name: "engineering" },
-      { value: "human resources", name: "human resources" },
-      { value: "legal", name: "legal" },
-    ],
-  };
-  const states = {
-    label: "state",
-    options: [
-      {
-        name: "Alabama",
-        value: "AL",
-      },
-      {
-        name: "Alaska",
-        value: "AK",
-      },
-      {
-        name: "American Samoa",
-        value: "AS",
-      },
-      {
-        name: "Arizona",
-        value: "AZ",
-      },
-      {
-        name: "Arkansas",
-        value: "AR",
-      },
-      {
-        name: "California",
-        value: "CA",
-      },
-      {
-        name: "Colorado",
-        value: "CO",
-      },
-      {
-        name: "Connecticut",
-        value: "CT",
-      },
-      {
-        name: "Delaware",
-        value: "DE",
-      },
-      {
-        name: "District Of Columbia",
-        value: "DC",
-      },
-      {
-        name: "Federated States Of Micronesia",
-        value: "FM",
-      },
-      {
-        name: "Florida",
-        value: "FL",
-      },
-      {
-        name: "Georgia",
-        value: "GA",
-      },
-      {
-        name: "Guam",
-        value: "GU",
-      },
-      {
-        name: "Hawaii",
-        value: "HI",
-      },
-      {
-        name: "Idaho",
-        value: "ID",
-      },
-      {
-        name: "Illinois",
-        value: "IL",
-      },
-      {
-        name: "Indiana",
-        value: "IN",
-      },
-      {
-        name: "Iowa",
-        value: "IA",
-      },
-      {
-        name: "Kansas",
-        value: "KS",
-      },
-      {
-        name: "Kentucky",
-        value: "KY",
-      },
-      {
-        name: "Louisiana",
-        value: "LA",
-      },
-      {
-        name: "Maine",
-        value: "ME",
-      },
-      {
-        name: "Marshall Islands",
-        value: "MH",
-      },
-      {
-        name: "Maryland",
-        value: "MD",
-      },
-      {
-        name: "Massachusetts",
-        value: "MA",
-      },
-      {
-        name: "Michigan",
-        value: "MI",
-      },
-      {
-        name: "Minnesota",
-        value: "MN",
-      },
-      {
-        name: "Mississippi",
-        value: "MS",
-      },
-      {
-        name: "Missouri",
-        value: "MO",
-      },
-      {
-        name: "Montana",
-        value: "MT",
-      },
-      {
-        name: "Nebraska",
-        value: "NE",
-      },
-      {
-        name: "Nevada",
-        value: "NV",
-      },
-      {
-        name: "New Hampshire",
-        value: "NH",
-      },
-      {
-        name: "New Jersey",
-        value: "NJ",
-      },
-      {
-        name: "New Mexico",
-        value: "NM",
-      },
-      {
-        name: "New York",
-        value: "NY",
-      },
-      {
-        name: "North Carolina",
-        value: "NC",
-      },
-      {
-        name: "North Dakota",
-        value: "ND",
-      },
-      {
-        name: "Northern Mariana Islands",
-        value: "MP",
-      },
-      {
-        name: "Ohio",
-        value: "OH",
-      },
-      {
-        name: "Oklahoma",
-        value: "OK",
-      },
-      {
-        name: "Oregon",
-        value: "OR",
-      },
-      {
-        name: "Palau",
-        value: "PW",
-      },
-      {
-        name: "Pennsylvania",
-        value: "PA",
-      },
-      {
-        name: "Puerto Rico",
-        value: "PR",
-      },
-      {
-        name: "Rhode Island",
-        value: "RI",
-      },
-      {
-        name: "South Carolina",
-        value: "SC",
-      },
-      {
-        name: "South Dakota",
-        value: "SD",
-      },
-      {
-        name: "Tennessee",
-        value: "TN",
-      },
-      {
-        name: "Texas",
-        value: "TX",
-      },
-      {
-        name: "Utah",
-        value: "UT",
-      },
-      {
-        name: "Vermont",
-        value: "VT",
-      },
-      {
-        name: "Virgin Islands",
-        value: "VI",
-      },
-      {
-        name: "Virginia",
-        value: "VA",
-      },
-      {
-        name: "Washington",
-        value: "WA",
-      },
-      {
-        name: "West Virginia",
-        value: "WV",
-      },
-      {
-        name: "Wisconsin",
-        value: "WI",
-      },
-      {
-        name: "Wyoming",
-        value: "WY",
-      },
-    ],
-  };
-
   function openModal() {
     setIsOpen(true);
-  }
-
-  function afterOpenModal() {
-    // references are now sync'd and can be accessed.
-    subtitle.style.color = "#f00";
   }
 
   function closeModal() {
@@ -345,12 +90,20 @@ export default function CreateEmployee() {
           <DatePicker
             selected={birthDate}
             onChange={(date) => setBirthDate(date)}
+            peekNextMonth
+            showMonthDropdown
+            showYearDropdown
+            dropdownMode="select"
           />
 
           <label htmlFor="start-date">Start Date</label>
           <DatePicker
             selected={startDate}
             onChange={(date) => setStartDate(date)}
+            peekNextMonth
+            showMonthDropdown
+            showYearDropdown
+            dropdownMode="select"
           />
 
           <fieldset className="address">
@@ -387,7 +140,6 @@ export default function CreateEmployee() {
       </div>
       <Modal
         isOpen={modalIsOpen}
-        onAfterOpen={afterOpenModal}
         onRequestClose={closeModal}
         contentLabel="Response Modal"
         className="modal"
@@ -401,8 +153,7 @@ export default function CreateEmployee() {
 
         {isEmpty ? (
           <p>
-            One of the field is empty or wasn't selected, don't forget to select
-            the right state / department even if the preview showed it selected
+            At least one of the fields is empty or wasn't selected.
           </p>
         ) : (
           <p>Employee Created!</p>
